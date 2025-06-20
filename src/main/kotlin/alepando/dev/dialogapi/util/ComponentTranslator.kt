d41daa0b7c79b3ee9efa01da6414732dde421585
Alepando <aocamporam@gmail.com>
2025-06-20 03:00:43 -0600

    refactored ComponentTranslator to use PaperAdventure#asVanilla

--- src/main/kotlin/alepando/dev/dialogapi/util/ComponentTranslator.kt
+++ src/main/kotlin/alepando/dev/dialogapi/util/ComponentTranslator.kt
@@ -1,5 +1,6 @@
 package alepando.dev.dialogapi.util
 
+import io.papermc.paper.adventure.PaperAdventure
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.TextComponent
 import net.kyori.adventure.text.format.TextDecoration
@@ -10,35 +11,6 @@ import net.minecraft.network.chat.Style as NMSStyle
 object ComponentTranslator {
 
     fun toNMS(component: Component): NMSComponent {
-        val base = when (component) {
-            is TextComponent -> NMSComponent.literal(component.content())
-            else -> NMSComponent.literal("N/D")
-        }
-
-        base.style = convertStyle(component)
-
-        component.children().forEach {
-            base.append(toNMS(it))
-        }
-
-        return base
-    }
-
-    private fun convertStyle(component: Component): NMSStyle {
-        val style = component.style()
-        var nmsStyle = NMSStyle.EMPTY
-
-
-        style.color()?.let { color ->
-            nmsStyle = nmsStyle.withColor(ChatFormatting.getById(color.asHexString().removePrefix("#").toInt(16)))
-        }
-
-        if (style.hasDecoration(TextDecoration.BOLD)) nmsStyle = nmsStyle.withBold(true)
-        if (style.hasDecoration(TextDecoration.ITALIC)) nmsStyle = nmsStyle.withItalic(true)
-        if (style.hasDecoration(TextDecoration.UNDERLINED)) nmsStyle = nmsStyle.withUnderlined(true)
-        if (style.hasDecoration(TextDecoration.STRIKETHROUGH)) nmsStyle = nmsStyle.withStrikethrough(true)
-        if (style.hasDecoration(TextDecoration.OBFUSCATED)) nmsStyle = nmsStyle.withObfuscated(true)
-
-        return nmsStyle
+        return PaperAdventure.asVanilla(component)
     }
 }
