ad389616ff5170202610044f3ddcf0501eabe7ed
Alepando <aocamporam@gmail.com>
2025-06-20 02:36:51 -0600

    dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,44 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.1.21"
+    `java-library`
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
 }
 
-group = "dev.alepando"
-version = "1.0-SNAPSHOT"
+group = "alepando.dev"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
+    maven("https://maven.enginehub.org/repo/")
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.opencollab.dev/main/") {
+        name = "opencollab-snapshot"
+    }
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    paperweight.paperDevBundle("1.21.6-R0.1-SNAPSHOT")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+val targetJavaVersion = 21
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+        mergeServiceFiles()
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
 }
